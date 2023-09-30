4066b22cea0072c39eb86ddbc96cb8de68777183
yannemal <56878180+Yannemal@users.noreply.github.com>
2023-09-30 19:31:13 +0800

    async await

--- CupCakeCorner/ContentView.swift
+++ CupCakeCorner/ContentView.swift
@@ -3,45 +3,59 @@
 //  CupCakeCorner Project 10 Day49
 //  @TwoStraws - Paul Hudson's 100DaysOfSwiftUI
 //  Created by yannemal on 29/09/2023.
-// hello
+//
 
 import SwiftUI
 // MARK: - other class / structs
-class User: ObservableObject, Codable {
 
-    enum CodingKeys: CodingKey {
-        case name
-    }
-    
-    @Published var name = "Yann Guyt "
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-    }
-    
+struct Response : Codable {
+    var results : [Result]
 }
 
+struct Result : Codable {
+    var trackID : Int
+    var trackName : String
+    var collectionName : String
+}
 
 struct ContentView: View {
 // MARK: - DATA:
+    @State private var results = [Result]()
 
     var body: some View {
 // MARK: - someView
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+   
+        List(results, id: \.trackID) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
         }
-        .padding()
+        .task {
+            await loadData()
+        }
+        
     } //end someView
 // MARK: - METHODS
+    
+    func loadData() async {
+        guard let url = URL(string: /* API call */ "https://itunes.apple.com/search?term=taylor+swift&entity=song") else 
+        {
+            print("invalid URL")
+            return
+        }
+        // a sleep might occur an error might occur
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("invalid Data")
+        }
+    }
     
 } //end struct
 
